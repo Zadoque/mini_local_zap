333d4ae29ff4500ad8a31cef1a7163997d3f4159
Zadoque <zadoquepiresdedeus@gmail.com>
2026-04-27 09:10:17 -0300

    update ip for local test

--- cliente/src/config.mjs
+++ cliente/src/config.mjs
@@ -1,7 +1,7 @@
 import { readFileSync, writeFileSync, existsSync } from 'fs';
 
 export const CONFIG_FILE = './chat-config.json';
-export const config = { ip: '127.0.0.1', porta: '8080' };
+export const config = { ip: '192.168.18.9', porta: '8080' };
 
 export function carregaConfig() {
     try {
